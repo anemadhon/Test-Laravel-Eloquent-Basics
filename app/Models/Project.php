17d1730dec37af8fbd40a5bef39d686d82dc427e
Muhammad Nur Ramadhan Alam <50946665+anemadhon@users.noreply.github.com>
2021-12-07 10:20:44 +0700

    Update Project.php

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -11,4 +11,11 @@ class Project extends Model
     use HasFactory, SoftDeletes;
     
     protected $fillable = ['name'];
+    
+    protected static function booted()
+    {
+        static::created(function ($project) {
+            Stat::query()->increment('projects_count');
+        });
+    }
 }
